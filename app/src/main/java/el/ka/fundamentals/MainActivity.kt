d66e9f39e599b9120ba7f39ccb383b4a677b536a
Ekaterina-El <hackerrussia@yandex.ru>
2021-11-07 13:47:45 +0500

    Custom toast

--- app/src/main/java/el/ka/fundamentals/MainActivity.kt
+++ app/src/main/java/el/ka/fundamentals/MainActivity.kt
@@ -2,10 +2,22 @@ package el.ka.fundamentals
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.custom_toast.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        showToast.setOnClickListener {
+            Toast(this).apply {
+                duration = Toast.LENGTH_SHORT
+                view = layoutInflater.inflate(R.layout.custom_toast, clToast)
+                show()
+            }
+        }
     }
 }
