5cb993e1bead9fb51fc74351f097d329a1e80ef8
Ekaterina-El <hackerrussia@yandex.ru>
2021-11-07 20:43:00 +0500

    Alert dialogs

--- app/src/main/java/el/ka/fundamentals/MainActivity.kt
+++ app/src/main/java/el/ka/fundamentals/MainActivity.kt
@@ -2,25 +2,56 @@ package el.ka.fundamentals
 
 import android.os.Bundle
 import android.util.Log
-import android.view.Menu
-import android.view.MenuItem
-import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.toolbar_menu, menu)
-        return true
-    }
+        val addContactDialog = AlertDialog.Builder(this)
+            .setTitle("Add contact")
+            .setMessage("Do you want to add Mr. Poop to your contact list?")
+            .setIcon(R.drawable.ic_baseline_person_add_24)
+            .setPositiveButton("Yes") { _, _ ->
+                Log.d("Dialog", "You added Mr. Poop to your contact list")
+            }
+            .setNegativeButton("No") { _, _ ->
+                Log.d("Dialog", "You didn't add Mr. Poop to your contact list")
+            }.create()
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        Log.d("ItemTitle", item.title as String)
-        return true
+        btnDialog1.setOnClickListener { addContactDialog.show() }
+
+        val options = arrayOf("First Item", "Second Item", "Third Item")
+        val optionsDialog = AlertDialog.Builder(this)
+            .setTitle("Choose one of there options")
+            .setSingleChoiceItems(options, 0) { _, i ->
+                Log.d("Dialog", "You clicked on ${options[i]}")
+            }
+            .setPositiveButton("Accept") { _, i ->
+                Log.d("Dialog", "You accept")
+            }
+            .setNegativeButton("Cancel") { _, _ ->
+                Log.d("Dialog", "Canceled choose")
+            }
+            .create()
+        btnDialog2.setOnClickListener { optionsDialog.show() }
+
+        val multiChoiceDialog = AlertDialog.Builder(this)
+            .setTitle("Choose one of there options")
+            .setMultiChoiceItems(options, booleanArrayOf(true, true, false)) { _, i, b ->
+                Log.d("Dialog", "You clicked on ${options[i]}")
+            }
+            .setPositiveButton("Accept") { _, i ->
+                Log.d("Dialog", "You accept")
+            }
+            .setNegativeButton("Cancel") { _, _ ->
+                Log.d("Dialog", "Canceled choose")
+            }
+            .create()
+        btnDialog3.setOnClickListener { multiChoiceDialog.show() }
     }
 }
