f1c45d81f96a5887ce2a5f55510f4321e698a722
Ekaterina-El <hackerrussia@yandex.ru>
2021-11-16 19:21:27 +0500

    Notification with intent

--- app/src/main/java/el/ka/fundamentals/MainActivity.kt
+++ app/src/main/java/el/ka/fundamentals/MainActivity.kt
@@ -1,48 +1,60 @@
 package el.ka.fundamentals
 
-import android.content.SharedPreferences
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.TaskStackBuilder
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.getSystemService
 import kotlinx.android.synthetic.main.activity_main.*
 
-const val spFile = "spFile"
-
 class MainActivity : AppCompatActivity() {
-    lateinit var editSP: SharedPreferences.Editor
-    lateinit var sp: SharedPreferences
+    val CHANNEL_ID = "channelID"
+    val CHANNEL_NAME = "channelName"
+    val NOTIFICATION_ID = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        createNotificationChannel()
 
-        sp = getSharedPreferences(spFile, MODE_PRIVATE)
-        editSP = sp.edit()
-
-        save.setOnClickListener {
-            saveSP()
+        val intent = Intent(this, MainActivity::class.java)
+        val pendingIntent = TaskStackBuilder.create(this).run {
+            addNextIntentWithParentStack(intent)
+            getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT)
         }
 
-        load.setOnClickListener {
-            loadSP()
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle("Title of notification")
+            .setContentText("Content text")
+            .setSmallIcon(R.drawable.ic_star)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(pendingIntent)
+            .build()
+
+        val notificationManager = NotificationManagerCompat.from(this)
+
+        show_notification.setOnClickListener {
+            notificationManager.notify(NOTIFICATION_ID, notification)
         }
     }
 
-    private fun loadSP() {
-        et_name.setText(sp.getString("name", "empty"))
-        et_age.setText(sp.getInt("age", 0).toString())
-        is_adult.isChecked = sp.getBoolean("isAdult", false)
-    }
+    fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT).apply {
+                lightColor = Color.RED
+                enableLights(true)
+            }
 
-    private fun saveSP() {
-        val name = et_name.text.toString()
-        val age = et_age.text.toString().toInt()
-        val isAdult = is_adult.isChecked
-
-        editSP.apply {
-            putString("name", name)
-            putInt("age", age)
-            putBoolean("isAdult", isAdult)
-            apply()
+            val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            manager.createNotificationChannel(channel)
         }
     }
 }
