a5cd6492d81838002e379327fa1fbe347a87e3da
Ekaterina-El <hackerrussia@yandex.ru>
2021-11-17 17:53:19 +0500

    IntentService

--- app/src/main/java/el/ka/fundamentals/MainActivity.kt
+++ app/src/main/java/el/ka/fundamentals/MainActivity.kt
@@ -1,60 +1,25 @@
 package el.ka.fundamentals
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.PendingIntent
-import android.app.TaskStackBuilder
-import android.content.Context
 import android.content.Intent
-import android.graphics.Color
-import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
-import androidx.core.content.getSystemService
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    val CHANNEL_ID = "channelID"
-    val CHANNEL_NAME = "channelName"
-    val NOTIFICATION_ID = 0
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        createNotificationChannel()
 
-        val intent = Intent(this, MainActivity::class.java)
-        val pendingIntent = TaskStackBuilder.create(this).run {
-            addNextIntentWithParentStack(intent)
-            getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT)
-        }
-
-        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
-            .setContentTitle("Title of notification")
-            .setContentText("Content text")
-            .setSmallIcon(R.drawable.ic_star)
-            .setPriority(NotificationCompat.PRIORITY_HIGH)
-            .setContentIntent(pendingIntent)
-            .build()
-
-        val notificationManager = NotificationManagerCompat.from(this)
-
-        show_notification.setOnClickListener {
-            notificationManager.notify(NOTIFICATION_ID, notification)
-        }
-    }
-
-    fun createNotificationChannel() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT).apply {
-                lightColor = Color.RED
-                enableLights(true)
+        btn_start.setOnClickListener {
+            Intent(this, MyIntentService::class.java).also {
+                startService(it)
+                tv_status.text = "Service running"
             }
+        }
 
-            val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            manager.createNotificationChannel(channel)
+        btn_stop.setOnClickListener {
+            MyIntentService.stopService()
+            tv_status.text = "Service stopped"
         }
     }
 }
